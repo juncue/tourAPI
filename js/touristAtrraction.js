866922321ab8ed9b219250b05c1caeb343368568
juncue <junkyu83@gmail.com>
2025-02-15 00:31:10 +0900

    ajax 및 selectbox 출력 공통함수 구현

--- js/touristAtrraction.js
+++ js/touristAtrraction.js
@@ -6,102 +6,50 @@ const KEYWORD_BASE_SEARCH_URL = `http://apis.data.go.kr/B551011/KorService1/sear
 const REQUIRED_PARMAS = `?MobileOS=ETC&MobileApp=AppTest&serviceKey=${MY_KEY}&_type=json`;
 const DETAIL_PAGE_URL = `touristAttractionDetail.html`;
 
-$(function () {
-  getInitData();
-  getServiceCat1Data();
-  getAreaCat1Data();
-  $("#searchBtn").click(search);
-  $(".searchCancel-Btn").click(canselSearch);
-});
-
-$(document).on("change", "#serviceCat1", function () {
-  $(".serviceCat2").remove();
-  $(".serviceCat3").remove();
-  if ($(this).selectedIndex != 0) {
-    getServiceCat2Data($(this).val());
-  }
-});
-$(document).on("change", "#serviceCat2", function () {
-  $(".serviceCat3").remove();
-  if ($(this).selectedIndex != 0) {
-    getServiceCat3Data($(this).val());
-  }
-});
-$(document).on("change", "#areaCat1", function () {
-  $(".areaCat2").remove();
-  if ($(this).selectedIndex != 0) {
-    getAreaCat2Data($(this).val());
-  }
-});
-
-function search() {
-  $("#card").children().remove();
-  let serviceCat1 = $("#serviceCat1").val();
-  let serviceCat2 = $("#serviceCat2").val();
-  let serviceCat3 = $("#serviceCat3").val();
-  let areaCat1 = $("#areaCat1").val();
-  let areaCat2 = $("#areaCat2").val();
-  let searchWord = $("#searchWord").val();
-  if (searchWord == "") {
-    getSearchData(serviceCat1, serviceCat2, serviceCat3, areaCat1, areaCat2);
-  } else {
-    console.log(searchWord);
-    getKeywordData(
-      serviceCat1,
-      serviceCat2,
-      serviceCat3,
-      areaCat1,
-      areaCat2,
-      searchWord
-    );
-  }
-}
-
-function canselSearch() {
-  $(".serviceCat1").remove();
-  $(".serviceCat2").remove();
-  $(".serviceCat3").remove();
-  $(".areaCat1").remove();
-  $(".areaCat2").remove();
-  getServiceCat1Data();
-  getAreaCat1Data();
-  $("#searchWord").val("");
-}
-
-function getInitData() {
-  let url =
-    AREA_BASE_SEARCH_URL +
-    REQUIRED_PARMAS +
-    `&numOfRows=20&pageNo=1&listYN=Y&arrange=Q&contentTypeId=12`;
-  //   $(".loading").show();
+// ajax 요청 공통 함수
+function getData(url, callback) {
   $.ajax({
     url: url,
     type: "GET",
     dataType: "json",
     success: function (data) {
-      printTourist(data);
+      callback(data);
     },
     error: function (data) {
       console.log(data.responseText);
     },
     complete: function () {
-      //   $(".loading").hide();
     },
   });
 }
 
-function printTourist(json) {
+$(function () {
+  getInitCards(); // 페이지 로딩 시 전체 카드목록 조회
+  getServiceCat1Data(); // 서비스 분류 셀렉트 박스 중 대분류 항목 조회 
+  getAreaCat1Data();  // 지역 셀렉트 박스 중 광역시/도 항목 조회
+  $("#searchBtn").click(search);  // 검색 버튼 클릭 이벤트 리스너
+  $(".searchCancel-Btn").click(canselSearch);  // 검색초기화 버튼 클릭 이벤트 리스너
+});
+
+function getInitCards() {
+  let url =
+    AREA_BASE_SEARCH_URL +
+    REQUIRED_PARMAS +
+    `&numOfRows=20&pageNo=1&listYN=Y&arrange=Q&contentTypeId=12`;
+  //   $(".loading").show();
+  getData(url, printCards)
+}
+
+// ajax로 받아온 카드데이터를 출력해주는 함수
+function printCards(json) {
+  // $(".loading").hide();
   let output = "";
   let totalCount = json.response.body.totalCount;
   let item = json.response.body.items.item;
 
   $("#totalCount").html(totalCount);
   $.each(item, function (index, ele) {
-    console.log(ele);
-    let img = ele.firstimage;
-    if (img == "") {
-      img = `images/touristAttraction/no-image.jpeg`;
-    }
+    let img = (ele.firstimage != "")? ele.firstimage : `images/touristAttraction/no-image.jpeg`;
     let title = ele.title;
     let contentid = ele.contentid;
     let area1 = ele.addr1.split(" ")[0];
@@ -122,237 +70,117 @@ function printTourist(json) {
   $("#card").append(output);
 }
 
-function getAreaName(areacode, sigungucode, callback) {
-  let url =
-    AREA_CODE_URL + REQUIRED_PARMAS + `&areaCode=${areacode}&numOfRows=100`;
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType: "json",
-    success: function (data) {
-      let addrList = data.response.body.items.item;
-      let area = "";
-      $.each(addrList, function (index, ele) {
-        if (ele.code == sigungucode) {
-          area = ele.name;
-        }
-      });
-      callback(area);
-    },
-    error: function (data) {
-      console.log(data.responseText);
-    },
-    complete: function () {},
+// 셀렉트박스 옵션 출력에 대한 공통 함수
+function printSelectOptions(json, htmlSelector, className) {
+  let output = "";
+  let list = json.response.body.items.item;
+  $.each(list, function (index, ele) {
+    output += `<option class="${className}" value="${ele.code}">${ele.name}</option>`;
   });
+  $(htmlSelector).append(output);
 }
 
+// 서비스 분류 셀렉트박스 데이터 조회
 function getServiceCat1Data() {
   let url = SERVICE_CATEGORY_URL + REQUIRED_PARMAS + `&contentTypeId=12`;
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType: "json",
-    success: function (data) {
-      printServiceCat1(data);
-    },
-    error: function (data) {
-      console.log(data.responseText);
-    },
-    complete: function () {},
-  });
+  getData(url, function(data){
+    printSelectOptions(data, "#serviceCat1", "serviceCat1")
+  })
 }
-
 function getServiceCat2Data(code) {
   let url =
     SERVICE_CATEGORY_URL + REQUIRED_PARMAS + `&contentTypeId=12&cat1=${code}`;
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType: "json",
-    success: function (data) {
-      printServiceCat2(data);
-    },
-    error: function (data) {
-      console.log(data.responseText);
-    },
-    complete: function () {},
-  });
+  getData(url, function(data){
+    printSelectOptions(data, "#serviceCat2", "serviceCat2")
+  })
 }
-
 function getServiceCat3Data(code) {
   let url =
     SERVICE_CATEGORY_URL +
     REQUIRED_PARMAS +
     `&contentTypeId=12&cat1=${$("#serviceCat1").val()}&cat2=${code}`;
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType: "json",
-    success: function (data) {
-      printServiceCat3(data);
-    },
-    error: function (data) {
-      console.log(data.responseText);
-    },
-    complete: function () {},
-  });
+  getData(url, function(data){
+    printSelectOptions(data, "#serviceCat3", "serviceCat3")
+  })
 }
 
-function printServiceCat1(json) {
-  let output = "";
-  let list = json.response.body.items.item;
-  $.each(list, function (index, ele) {
-    let name = ele.name;
-    let code = ele.code;
-    output += `<option class="serviceCat1" value="${code}">${name}</option>`;
-  });
-  $("#serviceCat1").append(output);
-}
-
-function printServiceCat2(json) {
-  let output = "";
-  let list = json.response.body.items.item;
-  $.each(list, function (index, ele) {
-    let name = ele.name;
-    let code = ele.code;
-    output += `<option class="serviceCat2" value="${code}">${name}</option>`;
-  });
-  $("#serviceCat2").append(output);
-}
-
-function printServiceCat3(json) {
-  let output = "";
-  let list = json.response.body.items.item;
-  $.each(list, function (index, ele) {
-    let name = ele.name;
-    let code = ele.code;
-    output += `<option class="serviceCat3" value="${code}">${name}</option>`;
-  });
-  $("#serviceCat3").append(output);
-}
-
+// 지역 셀렉트박스 데이터 조회
 function getAreaCat1Data() {
   let url = AREA_CODE_URL + REQUIRED_PARMAS + `&numOfRows=50`;
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType: "json",
-    success: function (data) {
-      printAreaCat1(data);
-    },
-    error: function (data) {
-      console.log(data.responseText);
-    },
-    complete: function () {},
-  });
+  getData(url, function(data){
+    printSelectOptions(data, "#areaCat1", "areaCat1");
+  })
 }
-
 function getAreaCat2Data(code) {
   let url = AREA_CODE_URL + REQUIRED_PARMAS + `&areaCode=${code}&numOfRows=100`;
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType: "json",
-    success: function (data) {
-      printAreaCat2(data);
-    },
-    error: function (data) {
-      console.log(data.responseText);
-    },
-    complete: function () {},
-  });
+  getData(url, function(data){
+    printSelectOptions(data, "#areaCat2", "areaCat2");
+  })
 }
 
-function printAreaCat1(json) {
-  let output = "";
-  let list = json.response.body.items.item;
-  $.each(list, function (index, ele) {
-    let name = ele.name;
-    let code = ele.code;
-    output += `<option class="areaCat1" value="${code}">${name}</option>`;
-  });
-  $("#areaCat1").append(output);
-}
-
-function printAreaCat2(json) {
-  let output = "";
-  let list = json.response.body.items.item;
-  $.each(list, function (index, ele) {
-    let name = ele.name;
-    let code = ele.code;
-    output += `<option class="areaCat2" value="${code}">${name}</option>`;
-  });
-  $("#areaCat2").append(output);
-}
-
-function getKeywordData(
-  serviceCat1,
-  serviceCat2,
-  serviceCat3,
-  areaCat1,
-  areaCat2,
-  searchWord
-) {
-  let url =
-    KEYWORD_BASE_SEARCH_URL +
-    REQUIRED_PARMAS +
-    `&numOfRows=20&pageNo=1&listYN=Y&arrange=Q&contentTypeId=12&`;
-  if (areaCat1 != "noValue") url += `&areaCode=${areaCat1}`;
-  if (areaCat2 != "noValue") url += `&sigunguCode=${areaCat2}`;
-  if (serviceCat1 != "noValue") url += `&cat1=${serviceCat1}`;
-  if (serviceCat2 != "noValue") url += `&cat2=${serviceCat2}`;
-  if (serviceCat3 != "noValue") url += `&cat3=${serviceCat3}`;
-  url += `&keyword=${searchWord}`;
-  if (searchWord != "") {
-    url += `&keyword=${encodeURIComponent(searchWord)}`;
+// 셀렉트 박스 변경에 대한 이벤트리스너
+$(document).on("change", "#serviceCat1", function () {
+  $(".serviceCat2", ".serviceCat3").remove();
+  if ($(this).selectedIndex != 0) {
+    getServiceCat2Data($(this).val());
   }
-  //   $(".loading").show();
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType: "json",
-    success: function (data) {
-      console.log(data);
-      printTourist(data);
-    },
-    error: function (data) {
-      console.log(data.responseText);
-    },
-    complete: function () {
-      //   $(".loading").hide();
-    },
-  });
+});
+$(document).on("change", "#serviceCat2", function () {
+  $(".serviceCat3").remove();
+  if ($(this).selectedIndex != 0) {
+    getServiceCat3Data($(this).val());
+  }
+});
+$(document).on("change", "#areaCat1", function () {
+  $(".areaCat2").remove();
+  if ($(this).selectedIndex != 0) {
+    getAreaCat2Data($(this).val());
+  }
+});
+
+// 검색 실행
+function search() {
+  $("#card").empty();
+  let params = [
+    $("#areaCat1").val(),
+    $("#areaCat2").val(),
+    $("#serviceCat1").val(),
+    $("#serviceCat2").val(),
+    $("#serviceCat3").val()
+  ]
+  let keyword = $("#searchWord").val();
+  if (keyword != "") {
+    params.push(keyword);
+    let url = makeSearchUrl(KEYWORD_BASE_SEARCH_URL + REQUIRED_PARMAS, params);
+    getData(url, printCards);
+  } else {
+    let url = makeSearchUrl(AREA_BASE_SEARCH_URL + REQUIRED_PARMAS, params);
+    getData(url, printCards);
+  }
 }
 
-function getSearchData(
-  serviceCat1,
-  serviceCat2,
-  serviceCat3,
-  areaCat1,
-  areaCat2
-) {
-  let url =
-    AREA_BASE_SEARCH_URL +
-    REQUIRED_PARMAS +
-    `&numOfRows=20&pageNo=1&listYN=Y&arrange=Q&contentTypeId=12`;
-  if (areaCat1 != "noValue") url += `&areaCode=${areaCat1}`;
-  if (areaCat2 != "noValue") url += `&sigunguCode=${areaCat2}`;
-  if (serviceCat1 != "noValue") url += `&cat1=${serviceCat1}`;
-  if (serviceCat2 != "noValue") url += `&cat2=${serviceCat2}`;
-  if (serviceCat3 != "noValue") url += `&cat3=${serviceCat3}`;
-  //   $(".loading").show();
-  $.ajax({
-    url: url,
-    type: "GET",
-    dataType: "json",
-    success: function (data) {
-      printTourist(data);
-    },
-    error: function (data) {
-      console.log(data.responseText);
-    },
-    complete: function () {
-      //   $(".loading").hide();
-    },
-  });
+// 검색 쿼리스트링 만들어주는 함수
+function makeSearchUrl(url, params){
+  let paramNames = ["areaCode", "sigunguCode", "cat1", "cat2", "cat3", "keyword"]
+  $.each(params, function(index, ele){
+    if (ele != "noValue"){
+      url += `&${paramNames[index]}=${ele}`
+    }
+  }) 
+  return url;
 }
+
+// 검색 초기화 함수
+function canselSearch() {
+  $(".serviceCat1", ".serviceCat2",".serviceCat3").remove();
+  $(".areaCat1", "areaCat2").remove();
+  getServiceCat1Data();
+  getAreaCat1Data();
+  $("#searchWord").val("");
+}
+
+
+
+
+
+
