3e5eb9cf563716ffd89f45a8d970d6bba5aa3b90
juncue <junkyu83@gmail.com>
2025-02-16 14:20:09 +0900

    페이지네이션 기능 구현

--- js/touristAtrraction.js
+++ js/touristAtrraction.js
@@ -1,13 +1,22 @@
 const MY_KEY = `Mr%2FoLDV0QvesS1eTgQhWGB5QVE8m0cS4exeRvZdGXTV9HktCkhWBrEhPAMt2RYHN%2B2kvhbKkMka%2BK%2BgLlESbsA%3D%3D`;
+const CONTENT_TYPE_ID = 12;
 const AREA_CODE_URL = `http://apis.data.go.kr/B551011/KorService1/areaCode1`; // 3번 지역코드 조회
 const SERVICE_CATEGORY_URL = `http://apis.data.go.kr/B551011/KorService1/categoryCode1`; // 4번 서비스분류코드 조회
 const AREA_BASE_SEARCH_URL = `http://apis.data.go.kr/B551011/KorService1/areaBasedList1`; // 5번 지역기반 관광정보 조회
 const KEYWORD_BASE_SEARCH_URL = `http://apis.data.go.kr/B551011/KorService1/searchKeyword1`; // 7번 키워드 검색 조회
 const REQUIRED_PARMAS = `?MobileOS=ETC&MobileApp=AppTest&serviceKey=${MY_KEY}&_type=json`;
+const FOR_SEARCH_PARAMS = `&contentTypeId=${CONTENT_TYPE_ID}&listYN=Y&arrange=Q`
 const DETAIL_PAGE_URL = `touristAttractionDetail.html`;
 
+let numOfRows = 20;
+let PageGroupUnit = 10;
+
+let totalCount = 0;
+let totalPages = 0; 
+let currentPage = 1;
+
 // ajax 요청 공통 함수
-function getData(url, callback) {
+function requestData(url, callback) {
   $.ajax({
     url: url,
     type: "GET",
@@ -24,30 +33,60 @@ function getData(url, callback) {
 }
 
 $(function () {
-  getInitCards(); // 페이지 로딩 시 전체 카드목록 조회
+  getCards(currentPage); // 페이지 로딩 시 전체 카드목록 조회
   getServiceCat1Data(); // 서비스 분류 셀렉트 박스 중 대분류 항목 조회 
   getAreaCat1Data();  // 지역 셀렉트 박스 중 광역시/도 항목 조회
-  $("#searchBtn").click(search);  // 검색 버튼 클릭 이벤트 리스너
+  $("#searchBtn").click(function(){
+    currentPage = 1;
+    getCards(currentPage);
+    $("html, body").animate(
+      { scrollTop: $("#cardArea").offset().top },500
+    );
+  });  // 검색 버튼 클릭 이벤트 리스너
   $(".searchCancel-Btn").click(canselSearch);  // 검색초기화 버튼 클릭 이벤트 리스너
 });
 
-function getInitCards() {
-  let url =
-    AREA_BASE_SEARCH_URL +
-    REQUIRED_PARMAS +
-    `&numOfRows=20&pageNo=1&listYN=Y&arrange=Q&contentTypeId=12`;
-  //   $(".loading").show();
-  getData(url, printCards)
+// 카드를 가져오는 함수
+function getCards(pageNo) {
+  $("#card").empty();
+  let params = [
+    $("#areaCat1").val(),
+    $("#areaCat2").val(),
+    $("#serviceCat1").val(),
+    $("#serviceCat2").val(),
+    $("#serviceCat3").val()
+  ]
+  let keyword = $("#searchWord").val();
+  if (keyword != "") {
+    params.push(keyword);
+    let url = makeSearchUrl(KEYWORD_BASE_SEARCH_URL + REQUIRED_PARMAS + FOR_SEARCH_PARAMS + `&numOfRows=${numOfRows}&pageNo=${pageNo}`, params);
+    requestData(url, printCards);
+  } else {
+    let url = makeSearchUrl(AREA_BASE_SEARCH_URL + REQUIRED_PARMAS + FOR_SEARCH_PARAMS + `&numOfRows=${numOfRows}&pageNo=${pageNo}`, params);
+    requestData(url, printCards);
+  }
+}
+
+// 쿼리스트링 만들어주는 함수
+function makeSearchUrl(url, params){
+  let paramNames = ["areaCode", "sigunguCode", "cat1", "cat2", "cat3", "keyword"]
+  $.each(params, function(index, ele){
+    if (ele != "noValue"){
+      url += `&${paramNames[index]}=${ele}`
+    }
+  }) 
+  return url;
 }
 
 // ajax로 받아온 카드데이터를 출력해주는 함수
 function printCards(json) {
   // $(".loading").hide();
-  let output = "";
-  let totalCount = json.response.body.totalCount;
-  let item = json.response.body.items.item;
-
+  totalCount = json.response.body.totalCount;
+  totalPages = Math.ceil(totalCount / numOfRows);
   $("#totalCount").html(totalCount);
+  printPagination(); // 페이지 출력
+  let item = json.response.body.items.item;
+  let output = "";
   $.each(item, function (index, ele) {
     let img = (ele.firstimage != "")? ele.firstimage : `images/touristAttraction/no-image.jpeg`;
     let title = ele.title;
@@ -62,7 +101,7 @@ function printCards(json) {
             <div class="card-body">
               <small class="text-muted">${area1} | ${area2}</small>
               <h5 class="card-title"><a href="${DETAIL_PAGE_URL}?contentid=${contentid}">${title}</a></h5>
-              <span class="like"><i class="fa-regular fa-heart"></i></span>
+              <span id="${contentid}" class="like"><i class="fa-regular fa-heart"></i></span>
             </div>
           </div>
         </div>`;
@@ -82,15 +121,15 @@ function printSelectOptions(json, htmlSelector, className) {
 
 // 서비스 분류 셀렉트박스 데이터 조회
 function getServiceCat1Data() {
-  let url = SERVICE_CATEGORY_URL + REQUIRED_PARMAS + `&contentTypeId=12`;
-  getData(url, function(data){
+  let url = SERVICE_CATEGORY_URL + REQUIRED_PARMAS + `&contentTypeId=${CONTENT_TYPE_ID}`;
+  requestData(url, function(data){
     printSelectOptions(data, "#serviceCat1", "serviceCat1")
   })
 }
 function getServiceCat2Data(code) {
   let url =
-    SERVICE_CATEGORY_URL + REQUIRED_PARMAS + `&contentTypeId=12&cat1=${code}`;
-  getData(url, function(data){
+    SERVICE_CATEGORY_URL + REQUIRED_PARMAS + `&contentTypeId=${CONTENT_TYPE_ID}&cat1=${code}`;
+  requestData(url, function(data){
     printSelectOptions(data, "#serviceCat2", "serviceCat2")
   })
 }
@@ -98,8 +137,8 @@ function getServiceCat3Data(code) {
   let url =
     SERVICE_CATEGORY_URL +
     REQUIRED_PARMAS +
-    `&contentTypeId=12&cat1=${$("#serviceCat1").val()}&cat2=${code}`;
-  getData(url, function(data){
+    `&contentTypeId=${CONTENT_TYPE_ID}&cat1=${$("#serviceCat1").val()}&cat2=${code}`;
+  requestData(url, function(data){
     printSelectOptions(data, "#serviceCat3", "serviceCat3")
   })
 }
@@ -107,20 +146,20 @@ function getServiceCat3Data(code) {
 // 지역 셀렉트박스 데이터 조회
 function getAreaCat1Data() {
   let url = AREA_CODE_URL + REQUIRED_PARMAS + `&numOfRows=50`;
-  getData(url, function(data){
+  requestData(url, function(data){
     printSelectOptions(data, "#areaCat1", "areaCat1");
   })
 }
 function getAreaCat2Data(code) {
   let url = AREA_CODE_URL + REQUIRED_PARMAS + `&areaCode=${code}&numOfRows=100`;
-  getData(url, function(data){
+  requestData(url, function(data){
     printSelectOptions(data, "#areaCat2", "areaCat2");
   })
 }
 
 // 셀렉트 박스 변경에 대한 이벤트리스너
 $(document).on("change", "#serviceCat1", function () {
-  $(".serviceCat2", ".serviceCat3").remove();
+  $(".serviceCat2, .serviceCat3").remove();
   if ($(this).selectedIndex != 0) {
     getServiceCat2Data($(this).val());
   }
@@ -138,49 +177,55 @@ $(document).on("change", "#areaCat1", function () {
   }
 });
 
-// 검색 실행
-function search() {
-  $("#card").empty();
-  let params = [
-    $("#areaCat1").val(),
-    $("#areaCat2").val(),
-    $("#serviceCat1").val(),
-    $("#serviceCat2").val(),
-    $("#serviceCat3").val()
-  ]
-  let keyword = $("#searchWord").val();
-  if (keyword != "") {
-    params.push(keyword);
-    let url = makeSearchUrl(KEYWORD_BASE_SEARCH_URL + REQUIRED_PARMAS, params);
-    getData(url, printCards);
-  } else {
-    let url = makeSearchUrl(AREA_BASE_SEARCH_URL + REQUIRED_PARMAS, params);
-    getData(url, printCards);
-  }
-}
-
-// 검색 쿼리스트링 만들어주는 함수
-function makeSearchUrl(url, params){
-  let paramNames = ["areaCode", "sigunguCode", "cat1", "cat2", "cat3", "keyword"]
-  $.each(params, function(index, ele){
-    if (ele != "noValue"){
-      url += `&${paramNames[index]}=${ele}`
-    }
-  }) 
-  return url;
-}
-
 // 검색 초기화 함수
 function canselSearch() {
-  $(".serviceCat1", ".serviceCat2",".serviceCat3").remove();
-  $(".areaCat1", "areaCat2").remove();
+  $(".serviceCat1, .serviceCat2, .serviceCat3").remove();
+  $(".areaCat1, .areaCat2").remove();
   getServiceCat1Data();
   getAreaCat1Data();
   $("#searchWord").val("");
 }
 
+// 페이지 출력 함수
+function printPagination() {
+  $(".pagination").empty();
+  console.log(currentPage);
+  console.log(totalCount);
+  console.log(totalPages);
 
+  let currentPageGroup = Math.ceil(currentPage / PageGroupUnit); // 현재 페이지 그룹
+  let startPage = (currentPageGroup - 1) * PageGroupUnit + 1; // 그룹의 첫 페이지
+  let endPage = Math.min(startPage + PageGroupUnit - 1, totalPages); // 그룹의 마지막 페이지
+  // 이전 버튼 생성
+  if (currentPageGroup > 1) {
+    $(".pagination").append(`<li class="page-item" onclick="changePage(${startPage - 1})">
+            <a class="page-link" href="javascript:void(0)" aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </a>
+          </li>`);
+  }
+  // 페이지 번호 버튼 생성
+  for (let i = startPage; i <= endPage; i++) {
+    let activeClass = i == currentPage ? "active" : "";
+    $(".pagination").append(`<li class="page-item ${activeClass}" onclick="changePage(${i})"><a class="page-link" href="javascript:void(0)">${i}</a></li>`);
+  }
+  // 다음 버튼 생성
+  if (endPage < totalPages) {
+    $(".pagination").append(`<li class="page-item" onclick="changePage(${endPage + 1})">
+            <a class="page-link" href="javascript:void(0)" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+            </a>
+          </li>`);
+  }
+}
 
-
+function changePage(page) {
+  currentPage = page;
+  getCards(currentPage);
+  printPagination();
+  $("html, body").animate(
+    { scrollTop: $("#cardArea").offset().top },500
+  );
+}
 
 
