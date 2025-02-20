96da95a8e00e8e745e5e54805636245536d167ce
juncue <junkyu83@gmail.com>
2025-02-20 23:24:10 +0900

    목록 돌아가기

--- js/touristAttraction/touristAtrractionList.js
+++ js/touristAttraction/touristAtrractionList.js
@@ -19,11 +19,11 @@ $(function () {
     checkParams(url);
   } else {
     getCards(currentPage); // 페이지 로딩 시 전체 카드목록 조회
-    getServiceCat1Data(); // 서비스 분류 셀렉트 박스 중 대분류 항목 조회
-    getAreaCat1Data(); // 지역 셀렉트 박스 중 광역시/도 항목 조회
+    getServiceCat1Data(""); // 서비스 분류 셀렉트 박스 중 대분류 항목 조회
+    getAreaCat1Data(""); // 지역 셀렉트 박스 중 광역시/도 항목 조회
   }
   $("#searchBtn").click(function () {
-    history.replaceState({}, null, location.pathname);
+    // history.replaceState({}, null, location.pathname);
     currentPage = 1;
     getCards(currentPage);
     $("html, body").animate({ scrollTop: $("#cardArea").offset().top }, 500);
@@ -33,6 +33,7 @@ $(function () {
 
 // 카드를 가져오는 함수
 function getCards(pageNo) {
+  $(".spinner-border").show();
   console.log("::::::::::");
   $("#card").empty();
   // 쿼리스트링을 만들기 위해 검색 조건을 확인
@@ -69,39 +70,45 @@ function getCards(pageNo) {
 // ajax로 받아온 카드데이터를 출력해주는 함수
 function printCards(json) {
   console.log(json);
-  // $(".loading").hide();
+  $(".spinner-border").hide();
   totalCount = json.response.body.totalCount;
   totalPages = Math.ceil(totalCount / numOfRows);
-  $("#totalCount").html(totalCount);
-  printPagination(); // 페이지 출력
   let item = json.response.body.items.item;
   let output = "";
-  $.each(item, function (index, ele) {
-    let img =
-      ele.firstimage != ""
-        ? ele.firstimage
-        : `img/touristAttraction/no-image.jpeg`;
-    let title = ele.title;
-    let contentid = ele.contentid;
-    let area1 = ele.addr1.split(" ")[0];
-    let area2 = ele.addr1.split(" ")[1];
-    let likeArr = readCookie(); // 좋아요 체크 카드 표시
+  if (totalCount == 0) {
+    $("#totalCount").html(0);
+    $(".pagination").empty();
+    output = `<div class="resultNoting">검색 결과가 없습니다.</div>`
+  } else {
+    $("#totalCount").html(totalCount);
+    printPagination(); // 페이지 출력
+    $.each(item, function (index, ele) {
+      let img =
+        ele.firstimage != ""
+          ? ele.firstimage
+          : `img/touristAttraction/no-image.jpeg`;
+      let title = ele.title;
+      let contentid = ele.contentid;
+      let area1 = ele.addr1.split(" ")[0];
+      let area2 = ele.addr1.split(" ")[1];
+      let likeArr = readCookie(); // 좋아요 체크 카드 표시
 
-    output += `
-        <div class="col">
-          <div class="card h-100" >
-            <a href="javascript:void(0)"><img src="${img}" id="${contentid}" class="card-img-top" alt="${title}" onclick="goDetailPage(this);" /></a>
-            <div class="card-body">
-              <small class="text-muted">${area1} | ${area2}</small>
-              <h5 id="${contentid}" class="card-title" onclick="goDetailPage(this);"><a href="javascript:void(0);">${title}</a></h5>`;
+      output += `
+          <div class="col">
+            <div class="card h-100" >
+              <a href="javascript:void(0)"><img src="${img}" id="${contentid}" class="card-img-top" alt="${title}" onclick="goDetailPage(this);" /></a>
+              <div class="card-body">
+                <small class="text-muted">${area1} | ${area2}</small>
+                <h5 id="${contentid}" class="card-title" onclick="goDetailPage(this);"><a href="javascript:void(0);">${title}</a></h5>`;
 
-    if (likeArr.indexOf(contentid) != -1) {
-      output += `<span id="${contentid}" class="like" onclick="setLike(this);"><i class="fa-solid fa-heart"></i></span>`;
-    } else {
-      output += `<span id="${contentid}" class="like" onclick="setLike(this);"><i class="fa-regular fa-heart"></i></span>`;
-    }
-    output += `</div></div></div>`;
-  });
+      if (likeArr.indexOf(contentid) != -1) {
+        output += `<span id="${contentid}" class="like" onclick="setLike(this);"><i class="fa-solid fa-heart"></i></span>`;
+      } else {
+        output += `<span id="${contentid}" class="like" onclick="setLike(this);"><i class="fa-regular fa-heart"></i></span>`;
+      }
+      output += `</div></div></div>`;
+    });
+  }
   $("#card").append(output);
 }
 
@@ -116,47 +123,65 @@ function printSelectOptions(json, htmlSelector, className) {
 }
 
 // 서비스 분류 셀렉트박스 데이터 조회
-function getServiceCat1Data() {
+function getServiceCat1Data(cat1) {
   let url =
     SERVICE_CATEGORY_URL +
     REQUIRED_PARMAS +
     `&contentTypeId=${CONTENT_TYPE_ID}`;
   requestData(url, function (data) {
     printSelectOptions(data, "#serviceCat1", "serviceCat1");
+    if (cat1 != ""){
+      $("#serviceCat1 option").attr("selected", false); 
+      $("#serviceCat1").val(cat1).attr("selected", true);
+    }
   });
 }
-function getServiceCat2Data(code) {
+function getServiceCat2Data(cat1, cat2) {
   let url =
     SERVICE_CATEGORY_URL +
     REQUIRED_PARMAS +
-    `&contentTypeId=${CONTENT_TYPE_ID}&cat1=${code}`;
+    `&contentTypeId=${CONTENT_TYPE_ID}&cat1=${cat1}`;
   requestData(url, function (data) {
     printSelectOptions(data, "#serviceCat2", "serviceCat2");
+    if (cat2 != ""){
+      $("#serviceCat2 option").attr("selected", false); 
+      $("#serviceCat2").val(cat2).attr("selected", true);
+    }
   });
 }
-function getServiceCat3Data(code) {
+function getServiceCat3Data(cat1, cat2, cat3) {
   let url =
     SERVICE_CATEGORY_URL +
     REQUIRED_PARMAS +
-    `&contentTypeId=${CONTENT_TYPE_ID}&cat1=${$(
-      "#serviceCat1"
-    ).val()}&cat2=${code}`;
+    `&contentTypeId=${CONTENT_TYPE_ID}&cat1=${cat1}&cat2=${cat2}`;
   requestData(url, function (data) {
     printSelectOptions(data, "#serviceCat3", "serviceCat3");
+    if (cat3 != ""){
+      $("#serviceCat3 option").attr("selected", false); 
+      $("#serviceCat3").val(cat3).attr("selected", true);
+    }
   });
 }
 
 // 지역 셀렉트박스 데이터 조회
-function getAreaCat1Data() {
+function getAreaCat1Data(areaCode) {
   let url = AREA_CODE_URL + REQUIRED_PARMAS + `&numOfRows=50`;
   requestData(url, function (data) {
     printSelectOptions(data, "#areaCode", "areaCat1");
+    if (areaCode != ""){
+      $("#areaCode option").attr("selected", false); 
+      $("#areaCode").val(areaCode).attr("selected", true);
+    }
   });
 }
-function getAreaCat2Data(code) {
-  let url = AREA_CODE_URL + REQUIRED_PARMAS + `&areaCode=${code}&numOfRows=100`;
+function getAreaCat2Data(areaCode, sigunguCode) {
+  let url = AREA_CODE_URL + REQUIRED_PARMAS + `&areaCode=${areaCode}&numOfRows=100`;
   requestData(url, function (data) {
     printSelectOptions(data, "#sigunguCode", "areaCat2");
+    if (sigunguCode != ""){
+      $("#sigunguCode option").attr("selected", false); 
+      $("#sigunguCode").val(sigunguCode).attr("selected", true);
+    }
   });
 }
 
@@ -164,19 +189,19 @@ function getAreaCat2Data(code) {
 $(document).on("change", "#serviceCat1", function () {
   $(".serviceCat2, .serviceCat3").remove();
   if ($(this).selectedIndex != 0) {
-    getServiceCat2Data($(this).val());
+    getServiceCat2Data($(this).val(), "");
   }
 });
 $(document).on("change", "#serviceCat2", function () {
   $(".serviceCat3").remove();
   if ($(this).selectedIndex != 0) {
-    getServiceCat3Data($(this).val());
+    getServiceCat3Data($("#serviceCat1").val(), $(this).val(), "");
   }
 });
 $(document).on("change", "#areaCode", function () {
   $(".areaCat2").remove();
   if ($(this).selectedIndex != 0) {
-    getAreaCat2Data($(this).val());
+    getAreaCat2Data($(this).val(), "");
   }
 });
 
@@ -184,8 +209,8 @@ $(document).on("change", "#areaCode", function () {
 function canselSearch() {
   $(".serviceCat1, .serviceCat2, .serviceCat3").remove();
   $(".areaCat1, .areaCat2").remove();
-  getServiceCat1Data();
-  getAreaCat1Data();
+  getServiceCat1Data("");
+  getAreaCat1Data("");
   $("#searchWord").val("");
 }
 
@@ -269,11 +294,18 @@ function goDetailPage(card) {
 function checkParams(url) {
   console.log(url);
   let queryStr = url.split("?")[1];
-  let queryStrArr = queryStr.split("&");
-
+  let pageNo = getParameter("pageNo");
+  let areaCode = getParameter("areaCode");
+  let sigunguCode = getParameter("sigunguCode");
+  let cat1 = getParameter("cat1");
+  let cat2 = getParameter("cat2");
+  let cat3 = getParameter("cat3");
   let keyword = getParameter("keyword");
-  currentPage = queryStrArr[0].split("=")[1];
-  $("#searchWord").val(decodeURI(queryStrArr[6].split("=")[1]));
+  history.replaceState({}, null, location.pathname);
+
+  currentPage = pageNo;
+  $("#searchWord").val(decodeURI(keyword));
+
   if (keyword != "") {
     let url =
       KEYWORD_BASE_SEARCH_URL +
@@ -291,23 +323,22 @@ function checkParams(url) {
       `&numOfRows=${numOfRows}&${params}`;
     requestData(url, printCards);
   }
-
-  getAreaCat1Data();
-  getServiceCat1Data();
-  if (queryStrArr[1].split("=")[1] != "") {
-    console.log("=================");
-    getAreaCat2Data(queryStrArr[1].split("=")[1]);
-    console.log(queryStrArr[1].split("=")[1]);
-    console.log($("#areaCode option[value=noValue]"));
-    console.log(
-      $("#areaCode option[value=" + queryStrArr[1].split("=")[1] + "]")
-    );
-    $("#areaCode option[value=noValue]").attr("selected", false);
-    $("#areaCode option[value=noValue]").attr("selected", false);
-
-    $("#areaCode option[value=" + queryStrArr[1].split("=")[1] + "]").attr(
-      "selected",
-      true
-    );
+  if (areaCode != "") {
+    getAreaCat1Data(areaCode);
+    getAreaCat2Data(areaCode, "");
+  }
+  if (sigunguCode != "") {
+    getAreaCat2Data(areaCode, sigunguCode);
+  }
+  if (cat1 != "") {
+    getServiceCat1Data(cat1);
+    getServiceCat2Data(cat1, "");
+  }
+  if (cat2 != "") {
+    getServiceCat2Data(cat1, cat2);
+    getServiceCat3Data(cat1, cat2, "");
+  }
+  if (cat3 != "") {
+    getServiceCat3Data(cat1, cat2, cat3);
   }
 }
--- js/touristAttraction/touristAttractionDetail.js
+++ js/touristAttraction/touristAttractionDetail.js
@@ -121,7 +121,7 @@ function getDetailImage() {
     `&contentId=${contentId}&imageYN=Y&subImageYN=Y`;
   requestData(url, function (data) {
     console.log(data);
-    let items = data.response.body.items.item;
+    let items = data.response.body.items;
 
     printImage(items);
   });
@@ -152,36 +152,44 @@ function printInfo(title, value, id) {
 function printImage(data) {
   let carouselIndicators = "";
   let carouselInner = "";
-  $.each(data, function (index, ele) {
-    if (index == 0) {
-      carouselIndicators += `<button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="${index}"
-            class="active"
-            aria-current="true"
-            aria-label="Slide${index + 1}"
-          ></button>`;
+  if (data == "") {
+    printalternativeImage();
+  } else {
+    $.each(data.item, function (index, ele) {
+      if (index == 0) {
+        carouselIndicators += `<button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="${index}"
+              class="active"
+              aria-current="true"
+              aria-label="Slide${index + 1}"
+            ></button>`;
 
-      carouselInner += `<div class="carousel-item active">
-            <img src="${ele.originimgurl}" class="d-block w-100" alt="${ele.title}" />
-          </div>`;
-    } else {
-      carouselIndicators += `<button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="${index}"
-            aria-label="Slide${index + 1}"
-          ></button>`;
+        carouselInner += `<div class="carousel-item active">
+              <img src="${ele.originimgurl}" class="d-block w-100" alt="${ele.title}" />
+            </div>`;
+      } else {
+        carouselIndicators += `<button
+              type="button"
+              data-bs-target="#carouselExampleIndicators"
+              data-bs-slide-to="${index}"
+              aria-label="Slide${index + 1}"
+            ></button>`;
 
-      carouselInner += `<div class="carousel-item">
-            <img src="${ele.originimgurl}" class="d-block w-100" alt="${ele.title}" />
-          </div>`;
-    }
-  });
+        carouselInner += `<div class="carousel-item">
+              <img src="${ele.originimgurl}" class="d-block w-100" alt="${ele.title}" />
+            </div>`;
+      }
+    });
 
-  $(".carousel-indicators").html(carouselIndicators);
-  $(".carousel-inner").html(carouselInner);
+    $(".carousel-indicators").html(carouselIndicators);
+    $(".carousel-inner").html(carouselInner);
+  }
+}
+function printalternativeImage(){
+  let output = `<img width="100%" src="img/touristAttraction/no-image.jpeg">`
+  $("#carouselExampleIndicators").html(output);
 }
 
 function printMap(mapx, mapy) {
@@ -209,6 +217,7 @@ function printMap(mapx, mapy) {
   var zoomControl = new kakao.maps.ZoomControl();
   map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 }
+
 
 function setLike(likeIcon) {
   if (likeIcon.children[0].classList.contains("fa-regular")) {
